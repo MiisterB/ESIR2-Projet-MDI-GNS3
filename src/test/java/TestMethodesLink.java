15bb6cd3ac0882110e0e7184c6224f9d1a059843
Besma96 <kaouane.besma@gmail.com>
2019-05-29 16:47:34 +0200

    commit

--- src/test/java/TestMethodesLink.java
+++ src/test/java/TestMethodesLink.java
@@ -1,2 +1,44 @@
+
+import lib.Controller;
+import lib.Link;
+import lib.Project;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 public class TestMethodesLink {
+    /*
+    Test pour la classe link
+
+     */
+
+    @Test
+    public void testGetFirstNode() {
+        Controller controller = new Controller("148.60.11.161");
+        controller.deleteProject("testAuto");
+        Project project = controller.addProject("testAuto")
+                .getProject("testAuto");
+
+        List<Link> l= controller
+                .getProject("testAuto")
+                .addNode("n1", "vpcs", 0, 0)
+                .addNode("n2", "vpcs", 10, 10)
+                .addLink(project.getNode("n1"), project.getNode("n2"))
+                .getLinks();
+        if( l.isEmpty()){
+            assertNull(l);
+        }
+        else {
+            assertEquals(project.getNode("n1"), l.get(0).getFirstNode());
+
+        }
+        project.delete();
+    }
+
 }
--- src/test/java/TestMethodesNode.java
+++ src/test/java/TestMethodesNode.java
@@ -5,10 +5,15 @@ import lib.Project;
 import org.junit.Test;
 
 import java.util.List;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 public class TestMethodesNode {
 
+
+    /*
+    Test méthode de la classe noeud
+     */
 
     @Test
     public void testErrorWrongNodeType() throws InterruptedException {
@@ -34,7 +39,72 @@ public class TestMethodesNode {
     }
 
     @Test
-    public void testDuplicateNodes() throws InterruptedException {
+    public void testGetName_positif() {
+        Controller controller = new Controller("148.60.11.161");
+        controller.deleteProject("testAuto");
+        Project project = controller.addProject("testAuto")
+                .getProject("testAuto");
+
+        controller
+                .getProject("testAuto")
+                .addNode("n1", "vpcs", 0, 0);
+
+        assertEquals("n1", project.getNode("n1").getName());
+        project.delete();
+    }
+
+    @Test
+    public void testGetName_negatif() {
+        Controller controller = new Controller("148.60.11.161");
+        controller.deleteProject("testAuto");
+        Project project = controller.addProject("testAuto")
+                .getProject("testAuto");
+
+        String name = controller
+                .getProject("testAuto")
+                .addNode("n1", "vpcs", 0, 0)
+                .getNode("n1")
+                .getName();
+
+        assertNotEquals("ZAZA", name);
+        project.delete();
+
+    }
+
+    @Test
+    public void testGetId_positif() {
+        Controller controller = new Controller("148.60.11.161");
+        controller.deleteProject("testAuto");
+        Project project = controller.addProject("testAuto")
+                .getProject("testAuto");
+
+        controller
+                .getProject("testAuto")
+                .addNode("n1", "vpcs", 0, 0);
+
+        assertEquals("n1", project.getNode("n1").getId());
+        project.delete();
+
+    }
+
+    @Test
+    public void testGetId_negatif() {
+        Controller controller = new Controller("148.60.11.161");
+        controller.deleteProject("testAuto");
+        Project project = controller.addProject("testAuto")
+                .getProject("testAuto");
+
+        controller
+                .getProject("testAuto")
+                .addNode("n1", "vpcs", 0, 0);
+
+        assertNotEquals("kjfv", project.getNode("n1").getId());
+        project.delete();
+    }
+
+
+    @Test
+    public void testDuplicateNodes() {
 
         Controller controller = new Controller("148.60.11.161");
 
@@ -55,11 +125,11 @@ public class TestMethodesNode {
         assertEquals("V1", project.getNode("V1").getName());
         assertEquals("V2", project.getNode("V2").getName());
 
-        //project.delete();
+        project.delete();
     }
 
     @Test
-    public void testReloadNode() throws InterruptedException {
+    public void testReloadNode() {
 
         Controller controller = new Controller("148.60.11.161");
 
@@ -76,7 +146,7 @@ public class TestMethodesNode {
                 .getProject("TestReloadNode");
 
         assertEquals("V", project.getNode("V").getName());
-        //project.delete();
+        project.delete();
     }
 
 
@@ -97,9 +167,27 @@ public class TestMethodesNode {
         controller.getProject("TestStartStop").getNode("V").stopNode();
 
         Project project = controller
-                .getProject("TestGetLink");
+                .getProject("TestStartStop");
 
-        //assertEquals("V", project.getNode("V").getName());
-        //project.delete();
+        assertEquals("V", project.getNode("V").getName());
+        project.delete();
+    }
+
+    @Test
+    public void testSendCmdAndWaitResp() {
+
+        Controller controller = new Controller("148.60.11.161");
+        controller.deleteProject("testCmd");
+
+        String result = controller
+                .addProject("testCmd")
+                .getProject("testCmd")
+                .addNode("N", "vpcs")
+                .getNode("N")
+                .sendCmd("mkdir heyheyhey")
+                .sendCmdAndWaitResp("ls");
+
+        assertTrue(result.contains("heyheyhey"));
+
     }
 }
