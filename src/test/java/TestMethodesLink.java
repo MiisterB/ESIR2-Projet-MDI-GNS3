79aaea8627fb8b2d3263e0101022deb1c6427bd8
Besma96 <kaouane.besma@gmail.com>
2019-05-31 13:02:41 +0200

    tests unitaires class link ok

--- src/test/java/TestMethodesLink.java
+++ src/test/java/TestMethodesLink.java
@@ -8,81 +8,104 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class TestMethodesLink {
+
     /*
     Test pour la classe link
 
      */
 
     @Test
-    public void testGetFirstNode() {
-        Controller controller = new Controller("148.60.11.161");
-        controller.deleteProject("testAuto");
-        Project project = controller.addProject("testAuto")
-                .getProject("testAuto");
+    //tout tester d'un coup
+    public void main(){
+        testGetFirstNode();
+        testGetSecondNode();
+        testGetFirstPort();
+        testGetSecondPort();
+        testFilterAvailable();
+        testGetFirstNode2();
+        testGetFirstPort2();
+        testGetSecondNode2();
+        testGetSecondPort2();
+    }
 
+    Controller controller = new Controller("148.60.11.161");
+    Project project = creationProject();
+    List<Link> l = getLinks(project);
+
+    public Project creationProject(){
+
+        controller.deleteProject("testLink");
+        Project project = controller.addProject("testLink")
+                .getProject("testLink");
+        return project;
+    }
+    public List<Link> getLinks(Project project){
         List<Link> l= controller
-                .getProject("testAuto")
+                .getProject("testLink")
                 .addNode("n1", "vpcs", 0, 0)
                 .addNode("n2", "vpcs", 10, 10)
                 .addLink(project.getNode("n1"), project.getNode("n2"))
                 .getLinks();
+        return l;
+    }
+
+    @Test
+    public void testGetFirstNode() {
 
         assertEquals("n1", l.get(0).getFirstNode().getName());
-        project.delete();
+        //project.delete();
+    }
+
+    @Test
+    public void testGetFirstNode2() {
+        assertNotEquals("n2", l.get(0).getFirstNode().getName());
+        //project.delete();
     }
 
 
     @Test
     public void testGetSecondNode() {
-        Controller controller = new Controller("148.60.11.161");
-        controller.deleteProject("testAuto");
-        Project project = controller.addProject("testAuto")
-                .getProject("testAuto");
-
-        List<Link> l= controller
-                .getProject("testAuto")
-                .addNode("n1", "vpcs", 0, 0)
-                .addNode("n2", "vpcs", 10, 10)
-                .addLink(project.getNode("n1"), project.getNode("n2"))
-                .getLinks();
 
         assertEquals("n2", l.get(0).getSecondNode().getName());
-       project.delete();
+       //project.delete();
+    }
+
+    @Test
+    public void testGetSecondNode2() {
+        assertNotEquals("n1", l.get(0).getSecondNode().getName());
+        //project.delete();
     }
 
     @Test
     public void testGetFirstPort() {
-        Controller controller = new Controller("148.60.11.161");
-        controller.deleteProject("testAuto");
-        Project project = controller.addProject("testAuto")
-                .getProject("testAuto");
-
-        List<Link> l= controller
-                .getProject("testAuto")
-                .addNode("n1", "vpcs", 0, 0)
-                .addNode("n2", "vpcs", 10, 10)
-                .addLink(project.getNode("n1"), project.getNode("n2"))
-                .getLinks();
-
         assertEquals(0, l.get(0).getFirstNodePort());
-        project.delete();
+        //project.delete();
+    }
+
+    @Test
+    public void testGetFirstPort2() {
+        assertNotEquals(1, l.get(0).getFirstNodePort());
+        //project.delete();
+    }
+
+    @Test
+    public void testGetSecondPort() {
+        assertEquals(0, l.get(0).getFirstNodePort());
+        //project.delete();
+    }
+
+    @Test
+    public void testGetSecondPort2() {
+
+        assertNotEquals(1, l.get(0).getFirstNodePort());
+        //project.delete();
     }
 
     @Test
     public void testFilterAvailable() {
-        Controller controller = new Controller("148.60.11.161");
-        controller.deleteProject("testAuto");
-        Project project = controller.addProject("testAuto")
-                .getProject("testAuto");
-
-        List<Link> l= controller
-                .getProject("testAuto")
-                .addNode("n1", "vpcs", 0, 0)
-                .addNode("n2", "vpcs", 10, 10)
-                .addLink(project.getNode("n1"), project.getNode("n2"))
-                .getLinks();
 
         List<String> lo = l.get(0).available_filters();
         while(!lo.isEmpty()){
@@ -92,23 +115,9 @@ public class TestMethodesLink {
 
     }
 
-    @Test
-    public void testGetSecondPort() {
-        Controller controller = new Controller("148.60.11.161");
-        controller.deleteProject("testAuto");
-        Project project = controller.addProject("testAuto")
-                .getProject("testAuto");
 
-        List<Link> l= controller
-                .getProject("testAuto")
-                .addNode("n1", "vpcs", 0, 0)
-                .addNode("n2", "vpcs", 10, 10)
-                .addLink(project.getNode("n1"), project.getNode("n2"))
-                .getLinks();
 
-        assertEquals(0, l.get(0).getFirstNodePort());
-        //project.delete();
-    }
+
 
 
 
