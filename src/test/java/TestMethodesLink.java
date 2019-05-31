491ccbb22e0b2d96d2e6bc08134a972f73976c41
Besma96 <kaouane.besma@gmail.com>
2019-05-31 12:32:23 +0200

    nouveau test Links

--- src/test/java/TestMethodesLink.java
+++ src/test/java/TestMethodesLink.java
@@ -49,7 +49,68 @@ public class TestMethodesLink {
                 .getLinks();
 
         assertEquals("n2", l.get(0).getSecondNode().getName());
+       project.delete();
+    }
+
+    @Test
+    public void testGetFirstPort() {
+        Controller controller = new Controller("148.60.11.161");
+        controller.deleteProject("testAuto");
+        Project project = controller.addProject("testAuto")
+                .getProject("testAuto");
+
+        List<Link> l= controller
+                .getProject("testAuto")
+                .addNode("n1", "vpcs", 0, 0)
+                .addNode("n2", "vpcs", 10, 10)
+                .addLink(project.getNode("n1"), project.getNode("n2"))
+                .getLinks();
+
+        assertEquals(0, l.get(0).getFirstNodePort());
         project.delete();
     }
+
+    @Test
+    public void testFilterAvailable() {
+        Controller controller = new Controller("148.60.11.161");
+        controller.deleteProject("testAuto");
+        Project project = controller.addProject("testAuto")
+                .getProject("testAuto");
+
+        List<Link> l= controller
+                .getProject("testAuto")
+                .addNode("n1", "vpcs", 0, 0)
+                .addNode("n2", "vpcs", 10, 10)
+                .addLink(project.getNode("n1"), project.getNode("n2"))
+                .getLinks();
+
+        List<String> lo = l.get(0).available_filters();
+        while(!lo.isEmpty()){
+            System.out.print(lo.get(lo.size()-1));
+            lo.remove(lo.get(lo.size()-1));
+        }
+
+    }
+
+    @Test
+    public void testGetSecondPort() {
+        Controller controller = new Controller("148.60.11.161");
+        controller.deleteProject("testAuto");
+        Project project = controller.addProject("testAuto")
+                .getProject("testAuto");
+
+        List<Link> l= controller
+                .getProject("testAuto")
+                .addNode("n1", "vpcs", 0, 0)
+                .addNode("n2", "vpcs", 10, 10)
+                .addLink(project.getNode("n1"), project.getNode("n2"))
+                .getLinks();
+
+        assertEquals(0, l.get(0).getFirstNodePort());
+        //project.delete();
+    }
+
+
+
 
 }
